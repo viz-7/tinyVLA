c4baaadbe170e571321a26bb124848801f2992e5
Vishnu <SnowSneeze@protonmail.com>
2026-09-11 12:14:18 +0530

    init

---
+++ collect_data.sh
@@ -0,0 +1,7 @@
+python -m mani_skill.examples.motionplanning.panda.run \
+                                                 -e PushCube-v1 \
+                                                 -n 1000 \
+                                                 --only-count-success \
+                                                 -o rgb \
+                                                 --record-dir demos \
+                                                 --num-procs 8
